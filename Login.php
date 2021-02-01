baa83eeb79100d258d5d892bcf1b3f329d5b14da
Lamia Selmane <hl_selmane@esi.dz>
2021-02-01 14:33:56 +0100

    login done

--- Login.php
+++ Login.php
@@ -7,17 +7,21 @@ include "connexion.php";
     // username and password sent from form 
     $pwd=hash("md5",$_POST['pwd']);
     $email= $_POST['email'];
-    echo $pwd;
-    echo $email;
     
-    $sql = "SELECT * FROM utilisateur WHERE name_user = '$email'";
+    $sql = "SELECT id_user FROM utilisateur WHERE name_user = '$email' and hash_pwd = '$pwd'";
     $result = mysqli_query($db,$sql);
-    while ($row= $result->fetch_assoc()){
-        print_r($row) ;
-    }
+    $row= $result->fetch_assoc();
+    $count = mysqli_num_rows($result);
     
-    
+    // If result matched $myusername and $mypassword, table row must be 1 row
       
-
-
+    if($count == 1) {
+       
+       $_SESSION['email'] = $email;
+       
+       header("location: JS.html");
+    }else {
+        header("location: Login.html");
+    }
+    mysqli_close($db);
 ?>
