5bb3ccc7ff2b1a5a04cca2dce4487a8f4a3e6e14
Lamia Selmane <hl_selmane@esi.dz>
2021-02-01 14:58:12 +0100

    logout button done

--- Login.php
+++ Login.php
@@ -2,24 +2,18 @@
 session_start();
 include "connexion.php";
 
-
-
-    // username and password sent from form 
     $pwd=hash("md5",$_POST['pwd']);
     $email= $_POST['email'];
-    
     $sql = "SELECT id_user FROM utilisateur WHERE name_user = '$email' and hash_pwd = '$pwd'";
     $result = mysqli_query($db,$sql);
     $row= $result->fetch_assoc();
     $count = mysqli_num_rows($result);
-    
-    // If result matched $myusername and $mypassword, table row must be 1 row
-      
     if($count == 1) {
        
        $_SESSION['email'] = $email;
-       
-       header("location: JS.html");
+       header("location: JQuery.html");
+       exit();
+
     }else {
         header("location: Login.html");
     }
